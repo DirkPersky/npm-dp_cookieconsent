6107122cb89d7e0fe8404da7b635c16db36fff2b
dirk.persky <d.persky@gutenberghaus.de>
2019-12-13 10:48:01 +0100

    Basic Integration

---
+++ src/js/l10n/de.js
@@ -0,0 +1,14 @@
+const lang = {
+    "message": "Diese Website benutzt Cookies, die für den technischen Betrieb der Website erforderlich sind und stets gesetzt werden. Andere Cookies, um Inhalte und Anzeigen zu personalisieren und die Zugriffe auf unsere Website zu analysieren, werden nur mit Ihrer Zustimmung gesetzt. Außerdem geben wir Informationen zu Ihrer Verwendung unserer Website an unsere Partner für soziale Medien, Werbung und Analysen weiter.",
+    "dismiss": "Cookies zulassen!",
+    "allowall": "alle akzeptieren!",
+    "deny": "Ablehnen",
+    "policy": "Cookies",
+    "link": "Mehr Infos",
+    "dpRequire": "Notwendig",
+    "dpStatistik": "Statistiken",
+    "dpMarketing": "Marketing",
+    "media.notice": "Cookie-Hinweis",
+    "media.desc": "Durch das Laden dieser Ressource wird eine Verbindung zu externen Servern hergestellt, die Cookies und andere Tracking-Technologien verwenden, um die Benutzererfahrung zu personalisieren und zu verbessern. Weitere Informationen finden Sie in unserer Datenschutzerklärung.",
+    "media.btn": "Erlaube Cookies und lade diese Ressource",
+};
---
+++ src/js/l10n/en.js
@@ -0,0 +1,14 @@
+const lang = {
+    "message": "We use cookies and other tracking technologies to personalize and improve your experience. By continuing to use our website you consent to this.",
+    "dismiss": "allow cookies",
+    "allowall": "allow all!",
+    "deny": "decline",
+    "policy": "Cookie Policy",
+    "link": "More info",
+    "dpRequire": "necessary",
+    "dpStatistik": "statistics",
+    "dpMarketing": "marketing",
+    "media.notice": "Cookie Notice",
+    "media.desc": "Loading this resource will connect to external servers which use cookies and other tracking technologies to personalize and improve experience. Further information can be found in our privacy policy.",
+    "media.btn": "allow cookies and load this resource",
+};
